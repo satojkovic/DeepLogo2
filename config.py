2ef46c391afd9bc0fdcf03ce4cadd99edc6544ca
satojkovic <satojkovic@gmail.com>
2022-06-09 21:35:21 +0900

    Fix output directory for preprocessing

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 
 TRAIN_DIR = 'flickr_logos_27_dataset'
+DATA_DIR = 'data'
 IMAGES_DIR = os.path.join(TRAIN_DIR, 'flickr_logos_27_dataset_images')
 CROPPED_IMAGES_DIR = os.path.join(TRAIN_DIR, 'flickr_logos_27_dataset_images_cropped')
 ANNOT_FILE = os.path.join(TRAIN_DIR, 'flickr_logos_27_dataset_training_set_annotation.txt')
-CROPPED_ANNOT_FILE = os.path.join(TRAIN_DIR, 'flickr_logos_27_dataset_training_set_annotation_cropped.txt')
-CROPPED_ANNOT_FILE_TEST = os.path.join(TRAIN_DIR, 'flickr_logos_27_dataset_test_set_annotation_cropped.txt')
+CROPPED_ANNOT_FILE = os.path.join(DATA_DIR, 'flickr_logos_27_dataset_training_set_annotation_cropped.txt')
+CROPPED_ANNOT_FILE_TEST = os.path.join(DATA_DIR, 'flickr_logos_27_dataset_test_set_annotation_cropped.txt')
 
 CLASS_NAMES = [
     'Adidas', 'Apple', 'BMW', 'Citroen', 'Cocacola', 'DHL', 'Fedex', 'Ferrari',
