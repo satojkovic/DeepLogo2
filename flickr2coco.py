edcade6bab5b4b56e88c45bcb8c21a6279fd4e6a
satojkovic <satojkovic@gmail.com>
2022-08-03 22:08:52 +0900

    Add coco format file generator script

---
+++ flickr2coco.py
@@ -0,0 +1,28 @@
+from collections import OrderedDict
+import json
+
+
+def info():
+    tmp = OrderedDict()
+    tmp['description'] = 'Flickr logos 27 dataset'
+    tmp['url'] = 'http://image.ntua.gr/iva/datasets/flickr_logos/'
+    tmp['version'] = '1.0'
+    tmp['year'] = '2022'
+    tmp['contributor'] = 'satojkovic'
+    tmp['data_created'] = '2022/08/03'
+    return tmp
+
+
+if __name__ == '__main__':
+    query_list = ['info', 'licenses', 'images',
+                  'annotations', 'categories', 'segment_info']
+    js = OrderedDict()
+    for i, query in enumerate(query_list):
+        tmp = ''
+        if query == 'info':
+            tmp = info()
+
+        js[query] = tmp
+
+    with open('flickr_logos_27.json', 'w') as f:
+        json.dump(js, f, indent=2)
