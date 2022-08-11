89513abe4f2cc4ab7a32a1be2ab6084204217eaf
satojkovic <satojkovic@gmail.com>
2022-08-11 22:49:40 +0900

    Add new script

---
+++ delete_head_and_save.py
@@ -0,0 +1,16 @@
+import torch
+
+if __name__ == '__main__':
+    # Get pretrained weights
+    checkpoint = torch.hub.load_state_dict_from_url(
+        url='https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth',
+        map_location='cpu',
+        check_hash=True)
+
+    # Remove class weights
+    del checkpoint["model"]["class_embed.weight"]
+    del checkpoint["model"]["class_embed.bias"]
+
+    # Save
+    torch.save(checkpoint,
+               'detr-r50_no-class-head.pth')
